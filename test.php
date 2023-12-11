5b85acb863e75046c31cf210247ad7f5a60ad840
baptiste-boimard <bouketin27@gmail.com>
2023-12-11 09:30:20 +0100

    First

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+$userAge = 17;
+echo "Bonjour j'ai $userAge"
+?>
+
+<?php 
+$isEnabled = false;
+
+if ($isEnabled == true) {
+    echo "it's ok";
+} 
+else {
+    echo "it's wrong";
+}
+?>
